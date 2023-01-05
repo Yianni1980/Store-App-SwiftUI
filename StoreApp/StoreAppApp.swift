0b77f8de9c03daaa73a27d62f9aeb926c4def4a0
Yianni1980 <ioagian1980@gmail.com>
2023-01-05 15:24:41 +0200

    Initial Commit

---
+++ StoreApp/StoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreAppApp.swift
+//  StoreApp
+//
+//  Created by ioannis on 5/1/23.
+//
+
+import SwiftUI
+
+@main
+struct StoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
