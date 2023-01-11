809c624c16d8bbd90978a5b7b63d7936bf30b567
Yianni1980 <ioagian1980@gmail.com>
2023-01-11 13:28:09 +0200

    use of generics network call

--- StoreApp/Clients/StoreHTTPClient.swift
+++ StoreApp/Clients/StoreHTTPClient.swift
@@ -16,43 +16,112 @@ enum NetworkError:Error {
     
     
 }
+enum HttpMethod {
+    case get([URLQueryItem])
+    case post(Data?)
+    case delete
+    
+    var name:String {
+        switch self {
+            
+        case .get:
+            return "GET"
+        case .post:
+            return "POST"
+        case .delete:
+            return "DELETE"
+        }
+        
+    }
+}
 
+struct Resource<T:Codable> {
+    let url:URL
+    var headers:[String:String] = [:]
+    var method: HttpMethod = .get([])
+    
+}
 
 class StoreHTTPClient {
-    
-    func getCategories(url:URL) async throws ->[Category] {
+     //generic way
+    func load <T:Codable>(_ resource:Resource<T>) async throws ->T {
         
-       let (data,response) = try await URLSession.shared.data(from: url)
-        guard let httpResponse = response as? HTTPURLResponse,
-              httpResponse.statusCode == 200 else {
-                
-             throw  NetworkError.invalidResponse
+        var request = URLRequest(url: resource.url)
+        request.allHTTPHeaderFields = resource.headers
+        request.httpMethod = resource.method.name
+        
+        switch resource.method {
+            
+        case .get(let queryItems):
+            var components = URLComponents(url: resource.url, resolvingAgainstBaseURL: true)
+            components?.queryItems = queryItems
+            guard let url = components?.url else {
+                throw  NetworkError.badURL
+            }
+            request.url  = url
+        case .post(_):
+            break
+            
+        case .delete:
+            break
             
         }
         
-        guard let categories = try? JSONDecoder().decode([Category].self, from: data) else {
-            
+        let configuration = URLSessionConfiguration.default
+        configuration.httpAdditionalHeaders = ["Content-type":"application/json"]
+        
+        let session = URLSession(configuration: configuration)
+        
+        let (data,response) = try await session.data (for:request)
+        guard let httpResponse = response as? HTTPURLResponse,
+              httpResponse.statusCode == 200 || httpResponse.statusCode == 201 else {
+            throw NetworkError.invalidResponse
+        }
+        
+        guard let result = try? JSONDecoder().decode(T.self, from: data) else {
             throw NetworkError.decodingError
         }
         
-        return categories
+        return result
+        
     }
-    
-    
-    func getProductsByCategory(url:URL) async throws -> [Product] {
-        let (data,response) = try await URLSession.shared.data(from: url)
+      //old way
+//    func getCategories(url:URL) async throws ->[Category] {
+//
+//       let (data,response) = try await URLSession.shared.data(from: url)
+//        guard let httpResponse = response as? HTTPURLResponse,
+//              httpResponse.statusCode == 200 else {
+//
+//             throw  NetworkError.invalidResponse
+//
+//        }
+//
+//        guard let categories = try? JSONDecoder().decode([Category].self, from: data) else {
+//
+//            throw NetworkError.decodingError
+//        }
+//
+//        return categories
+//    }
+//
 
-        guard let httpResponse = response as? HTTPURLResponse,
-              httpResponse.statusCode == 200 else {
-                
-             throw  NetworkError.invalidResponse
-            
-        }
-        guard let products = try? JSONDecoder().decode([Product].self, from: data) else {
-            
-            throw NetworkError.decodingError
-        }
-        
-        return products
-    }
+
+    // old way
+
+//    func getProductsByCategory(url:URL) async throws -> [Product] {
+//        let (data,response) = try await URLSession.shared.data(from: url)
+//
+//        guard let httpResponse = response as? HTTPURLResponse,
+//              httpResponse.statusCode == 200 else {
+//
+//             throw  NetworkError.invalidResponse
+//
+//        }
+//        guard let products = try? JSONDecoder().decode([Product].self, from: data) else {
+//
+//            throw NetworkError.decodingError
+//        }
+//
+//        return products
+//    }
 }
--- StoreApp/Models/StoreModel.swift
+++ StoreApp/Models/StoreModel.swift
@@ -15,14 +15,19 @@ class StoreModel:ObservableObject  {
     @Published private (set)var products:[Product]=[]
     
     func fetchCategories() async throws {
+        //generic
+       categories =  try await client.load(Resource(url: URL.allCategories))
         
-         categories = try await client.getCategories(url: URL.allCategories)
+       //  categories = try await client.getCategories(url: URL.allCategories)
         
     }
     
     func fetchProductsByCategory(_ categoryId:Int) async throws {
+                //generic
+
+            products =  try await client.load(Resource(url: URL.productsByCategory(categoryId)))
         
-       products = try await client.getProductsByCategory(url: URL.productsByCategory(categoryId))
+       //products = try await client.getProductsByCategory(url: URL.productsByCategory(categoryId))
     }
     
 }
